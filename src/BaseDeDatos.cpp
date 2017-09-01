2211994dc0e0e3c8887350d989d308c255910f8f
Cristian Kubrak <kubrakcristian@gmail.com>
2017-09-01 16:04:51 -0300

    BaseDeDatos.tabla()

--- src/BaseDeDatos.cpp
+++ src/BaseDeDatos.cpp
@@ -6,3 +6,11 @@ BaseDeDatos::BaseDeDatos (vector<pair<Tabla,string> > tablas)
 void BaseDeDatos::agregarTabla(const Tabla &T, string nombre){
   _tablas.push_back(make_pair(T, nombre));
 }
+
+vector<Tabla> BaseDeDatos::tablas(){
+  vector<Tabla> resultado;
+  for (unsigned int i = 0; i < _tablas.size(); ++i){
+      resultado.push_back(_tablas[i].first);
+  }
+         return resultado;
+}
