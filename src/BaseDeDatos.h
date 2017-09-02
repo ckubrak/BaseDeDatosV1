de8b234f56da499b4c4b3cbc8da17130937c35ed
Cristian Kubrak <kubrakcristian@gmail.com>
2017-09-02 16:06:31 -0300

    operadores, otras operaciones, etc en BD

--- src/BaseDeDatos.h
+++ src/BaseDeDatos.h
@@ -10,22 +10,31 @@ class BaseDeDatos {
 
 public: 
 
+  /// @generador
 	BaseDeDatos(vector< pair<Tabla,string> > v);
 
+  /// @generador
 	void agregarTabla(const Tabla& T, string nombre);
 
+  /// @generador @otra op
 	void agregarENTabla(Tabla& T, Registro r);
 
+  /// @otra operacion
 	bool insercionValida(const Tabla& T, Registro r);
 
+  /// @otra operacion
 	void insercion(Tabla& T, Registro r);
 
+  /// @otra operacion
 	void criterioValido(const Tabla& T, Criterio c);
 
+  /// @otra operacion
 	Criterio criterioMasUsado();
 
+  /// @observador
 	vector<Tabla> tablas();
 
+  /// @otra operacion
 	int cantidadDeUsos(Criterio c);
 
 
