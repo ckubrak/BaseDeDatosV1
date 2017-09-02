1f2acd44fa32ea70b95ef1fa8a2b2191187fc2a5
Cristian Kubrak <kubrakcristian@gmail.com>
2017-09-02 16:14:53 -0300

    Agregue les const en BD.cpp

--- src/BaseDeDatos.cpp
+++ src/BaseDeDatos.cpp
@@ -7,7 +7,7 @@ void BaseDeDatos::agregarTabla(const Tabla &T, string nombre){
   _tablas.push_back(make_pair(T, nombre));
 }
 
-vector<Tabla> BaseDeDatos::tablas(){
+vector<Tabla> BaseDeDatos::tablas()const{
   vector<Tabla> resultado;
   for (unsigned int i = 0; i < _tablas.size(); ++i){
       resultado.push_back(_tablas[i].first);
@@ -15,10 +15,12 @@ vector<Tabla> BaseDeDatos::tablas(){
   return resultado;
 }
 
-int BaseDeDatos::cantidadDeUsos(Criterio c){
+int BaseDeDatos::cantidadDeUsos(Criterio c) const{
   for (unsigned int i = 0; i < _criterios.size(); ++i){
     if (_criterios[i].first == c)
       return _criterios[i].second;
   }
   return 0;
 }
+
+
