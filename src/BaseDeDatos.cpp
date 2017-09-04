203056464fead25c9d7a454274d6138a48110272
wrios@dc.uba.ar <kennedy.wrc@hotmail.com>
2017-09-04 12:55:20 -0300

    FaltaCriterioValido

--- src/BaseDeDatos.cpp
+++ src/BaseDeDatos.cpp
@@ -1,30 +1,63 @@
 #include "BaseDeDatos.h"
+#include "utils.h"
 
 BaseDeDatos::BaseDeDatos (vector<pair<Tabla,string> > tablas)
   : _tablas(tablas) {};
 
 void BaseDeDatos::agregarTabla(const Tabla &T, string nombre){
-  _tablas.push_back(make_pair(T, nombre));
+  bool esta = false;
+  for(int i = 0; i < tablas().size(); i++){
+    if(T == tablas()[i]) 
+      esta = true;
+  }
+  if(!esta) _tablas.push_back(make_pair(T, nombre));
+}
+
+void BaseDeDatos::agregarENTabla(Tabla& T, Registro r){
+  if(insercionValida(T,r)) T.agregarRegistro(r);
+}
+
+vector<Criterio> BaseDeDatos::criterios(){
+  vector<Criterio> resultado;
+  for (unsigned int i = 0; i < _criterios.size(); i++){
+      resultado.push_back(_criterios[i].first);
+  }
+  return resultado;
+}
+
+int BaseDeDatos::maximoUso(){
+  return _maximoUso;
 }
 
 vector<Tabla> BaseDeDatos::tablas() const{
   vector<Tabla> resultado;
-  for (unsigned int i = 0; i < _tablas.size(); ++i){
+  for (unsigned int i = 0; i < _tablas.size(); i++){
       resultado.push_back(_tablas[i].first);
   }
   return resultado;
 }
 
 int BaseDeDatos::cantidadDeUsos(Criterio c) const{
-  for (unsigned int i = 0; i < _criterios.size(); ++i){
+  for (unsigned int i = 0; i < _criterios.size(); i++){
     if (_criterios[i].first == c)
       return _criterios[i].second;
   }
   return 0;
 }
 
+void BaseDeDatos::criterioValido(const Tabla& T, Criterio c) const{
+  
+}
+
 vector<Criterio> BaseDeDatos::criteriosMasUsado() const {
   vector<Criterio> masUsados;
+  /*for(int i = 0; i < criterios().size(); i++){
+    if(criterios()[i].second == maximoUso()){
+      masUsados.push_back(criterios()[i].first);
+    }
+  }
+  */
+
   masUsados.push_back(_criterios[0].first);
   int cantVeces = _criterios[0].second;
 
@@ -38,4 +71,20 @@ vector<Criterio> BaseDeDatos::criteriosMasUsado() const {
       cantVeces = _criterios[i].second;
     }
   }
+  
+}
+
+
+bool BaseDeDatos::insercionValida(const Tabla& T, Registro r){
+  if(!seteq(T.campos(), r.campos())) return false;
+  vector<string> claves = T.claves();
+  vector<Registro> registros = T.registros();
+  for(int i = 0; i < claves.size(); i++){
+    for(int j = 0; j < registros.size(); j++){
+      if(registros[j].dato(claves[i]) == r.dato(claves[i])){
+        return false;
+      } 
+    }
+  }
+  return true;
 }
--- src/BaseDeDatos.h
+++ src/BaseDeDatos.h
@@ -11,37 +11,39 @@ class BaseDeDatos {
 public:
 
   /// @generador
-	BaseDeDatos(vector< pair<Tabla,string> > v);
+	BaseDeDatos(vector< pair<Tabla,string> > v);//L
 
   /// @generador
-	void agregarTabla(const Tabla& T, string nombre);
+	void agregarTabla(const Tabla& T, string nombre);//L
 
   /// @generador @otra op
-	void agregarENTabla(Tabla& T, Registro r);
+	void agregarENTabla(Tabla& T, Registro r);//L
 
   /// @otra operacion
-	bool insercionValida(const Tabla& T, Registro r);
-
-  /// @otra operacion
-	void insercion(Tabla& T, Registro r);
+	bool insercionValida(const Tabla& T, Registro r);//L
 
   /// @otra operacion
 	void criterioValido(const Tabla& T, Criterio c) const;
 
   /// @otra operacion
-	vector<Criterio> criteriosMasUsado() const;
+	vector<Criterio> criteriosMasUsado() const;//L
 
   /// @observador
-	vector<Tabla> tablas() const;
+	vector<Tabla> tablas() const;//L
 
   /// @otra operacion
-	int cantidadDeUsos(Criterio c) const;
+	int cantidadDeUsos(Criterio c) const;//L
+
+	int maximoUso();//L
+
+	vector<Criterio> criterios();//L
 
 
 private:
 
 	vector<pair<Criterio, int> > _criterios;
 	vector<pair<Tabla,string>> _tablas;
+	int _maximoUso;
 };
 
 
