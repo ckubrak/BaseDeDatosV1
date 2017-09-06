8f9b33cbba56cfec771e383d98cf2085795787f7
Cristian Kubrak <kubrakcristian@gmail.com>
2017-09-06 09:45:14 -0300

    comento temporalmente el test de agregarRegisto

--- tests/zbd_test.cpp
+++ tests/zbd_test.cpp
@@ -36,6 +36,17 @@ TEST(Zbd_test, igualdad){
 TEST(Zbd_test,agregarTabla){
   EXPECT_EQ(bd,bd);
   BaseDeDatos bdtest = bd;
+  EXPECT_NE(bdtest,bd2);
   bdtest.agregarTabla(t2, "Carreras");
   EXPECT_EQ(bdtest,bd2);
 }
+
+// TEST(Zbd_test, agregarRegistro){
+//   vector<string> campos = {"LU", "Año", "Nombre", "Carrera"};
+//   Registro r1(campos, {datoNat(181), datoNat(2017), datoStr("March"), datoStr("Comp")});
+//   BaseDeDatos bdtest = bd;
+//   EXPECT_EQ(bdtest.tablas()[0].registros().size(), 0);
+//   bdtest.agregarENTabla(t, r1);
+//   EXPECT_EQ(bdtest.tablas()[0].registros().size(), 1);
+
+// }
